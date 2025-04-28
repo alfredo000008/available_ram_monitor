6679f268f4d9ce001a9f086ee2864e62de6d3319
alfredo000008 <alfredogasa8@hotmail.com>
2025-04-27 20:59:16 -0600

    Fix username environment variable assignment and improve error handling in CI/CD script

--- scripts/msg_from_cicd.py
+++ scripts/msg_from_cicd.py
@@ -6,7 +6,7 @@ def main():
 
     if name is None:
         print("Name env variable could not be found.")
-
+        return
     print(f"Hello {name}. This is a message from the CI/CD pipeline.")
 
 if __name__ == "__main__":
